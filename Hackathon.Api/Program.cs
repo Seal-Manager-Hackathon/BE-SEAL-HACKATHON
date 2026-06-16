9125b94f3425f5275a23c2d2dfbf2902095f290d
Hoàng Phạmq <139544601+yuikiri@users.noreply.github.com>
2026-06-16 19:06:50 +0700

    sdf (#22)
    * fix add role in cookie
    
    * k
    
    * fix get me
    
    * k
    
    * h
    
    ---------
    
    Co-authored-by: duongbilly18012004@gmail.com <duongbilly18012004@gmail.com>

--- Hackathon.Api/Program.cs
+++ Hackathon.Api/Program.cs
@@ -96,10 +96,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("*")
+            .AllowAnyOrigin()
             .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+            .AllowAnyMethod();
+        // ĐÃ XÓA .AllowCredentials()
     });
 });
 var app = builder.Build();
