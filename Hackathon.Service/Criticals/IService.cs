7b28e827497641bc924db3ae720a4007fb2f75b6
phamquochoang356@gmail.com <phamquochoang356@gmail.com>
2026-06-30 03:37:30 +0700

    feat: implement criteria management service and add corresponding API documentation

--- Hackathon.Service/Criticals/IService.cs
+++ Hackathon.Service/Criticals/IService.cs
@@ -2,7 +2,7 @@ namespace Hackathon.Service.Criticals;
 
 public interface IService
 {
-    Task<Response.RoundCriteriaResponse> GetCriteriaByRound(Guid roundId);
+    Task<List<Response.RoundCriteriaResponse>> GetCriteriaByRound(Guid roundId);
     Task<List<Response.RoundCriteriaResponse>> GetCriteriaByEvent(Guid eventId);
     Task<Response.CreateCriteriaResponse> CreateCriteria(Guid eventId, Guid roundId, Request.CreateCriteriaRequest request);
     Task ActivateCriteria(Guid eventId, Guid roundId, Guid templateId);
